9b9636a5bda53416b96143157cfbed39176920a6
Tejas Ingle <tejas.ingle@somaiya.edu>
2024-03-04 18:37:59 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/pin")
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: Number,
+  boards:{
+    type:Array,
+    default:[]
+  },
+});
+
+userSchema.plugin(plm);
+module.exports = mongoose.model("user",userSchema);
